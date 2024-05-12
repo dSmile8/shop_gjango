982ab714e431571d80fdcd41d03334c330fc6e48
Сергей Арефьев <Simpson6515@yandex.ru>
2024-05-12 19:19:07 +0500

    added bloging app and realized CRUD.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, product_info, products_list
+from catalog.views import ProductDetailView, ProductListView, ContactsTemplateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', products_list, name='products_list'),
-    path('contacts/', contacts),
-    path('product/<int:pk>/', product_info, name='product_info')
+    path('', ProductListView.as_view(), name='product_list'),
+    path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_info')
 ]
--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,5 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
+    path('bloging/', include('bloging.urls', namespace='bloging')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
