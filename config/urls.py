96473cc5fa07f3ffe3aa96d62b2360f56ed7d10c
Сергей Арефьев <Simpson6515@yandex.ru>
2024-06-02 04:18:53 +0500

    fix dz 22_2

--- config/urls.py
+++ config/urls.py
@@ -2,18 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.contrib.auth import views as auth_views
-
-from users.views import CustomPasswordComplete
 
 urlpatterns = [
-    path('users/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('users/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('users/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('users/reset/done/', CustomPasswordComplete.as_view(), name='password_reset_complete'),
+                  # path('users/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+                  # path('users/password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+                  #      name='password_reset_done'),
+                  # path('users/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+                  #      name='password_reset_confirm'),
+                  # path('users/reset/complete/', CustomPasswordResetCompleteView.as_view(),
+                  #      name='password_reset_complete'),
 
-    path("admin/", admin.site.urls),
-    path('', include('catalog.urls', namespace='catalog')),
-    path('bloging/', include('bloging.urls', namespace='bloging')),
-    path('users/', include('users.urls', namespace='users')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path("admin/", admin.site.urls),
+                  path('', include('catalog.urls', namespace='catalog')),
+                  path('bloging/', include('bloging.urls', namespace='bloging')),
+                  path('users/', include('users.urls', namespace='users')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import UserCreateView, email_verification, ProfileView
+from users.views import UserCreateView, email_verification, ProfileView, reset_password
 
 app_name = UsersConfig.name
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', UserCreateView.as_view(), name='create'),
     path('email-confirm/<str:token>/', email_verification, name='email_confirm'),
-    path('profile/', ProfileView.as_view(), name='profile')
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path("reset_password/", reset_password, name="reset_password"),
 ]
